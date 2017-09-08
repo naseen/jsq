177810357bdd726a24caf153ad1ce4ecc5a6ca7b
naseen <31780711+naseen@users.noreply.github.com>
2017-09-08 23:36:20 +0200

    Create mq.js
    Mitt javascript

---
+++ mq.js
@@ -0,0 +1,33 @@
+    
+// Create a client instance
+client = new Paho.MQTT.Client("m20.cloudmqtt.com", Number(15809), "fbo");
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect},{password:"pass"},{userName:"user"});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("World");
+  message = new Paho.MQTT.Message("Hello");
+  message.destinationName = "World";
+  client.send(message);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
